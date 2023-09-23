7626d76ea6fe4e3e59a2fda00ef2786fe666f588
unknown <gkdnjs0508@naver.com>
2023-09-23 16:21:04 +0900

    first commit

---
+++ insta/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import Sub
+from content.views import Main, UploadFeed
+from .settings import MEDIA_ROOT, MEDIA_URL
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('main/', Main.as_view()),
+    path('content/', include('content.urls')),
+    path('user/', include('user.urls'))
+]
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
