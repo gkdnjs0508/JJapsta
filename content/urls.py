070e68c3aef17c093c75236a59ace583e9b8c636
unknown <gkdnjs0508@naver.com>
2023-09-23 16:35:45 +0900

    second

--- content/urls.py
+++ content/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import UploadFeed, Profile, Main, UploadReply, ToggleLike, ToggleBookmark
 
+
 urlpatterns = [
     path('upload', UploadFeed.as_view()),
     path('reply', UploadReply.as_view()),
--- insta/urls.py
+++ insta/urls.py
@@ -4,11 +4,13 @@ from .views import Sub
 from content.views import Main, UploadFeed
 from .settings import MEDIA_ROOT, MEDIA_URL
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
     path('content/', include('content.urls')),
+    path('', RedirectView.as_view(url='/main/')),
     path('user/', include('user.urls'))
 ]
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
